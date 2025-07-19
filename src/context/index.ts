1a36e53480bfa0acc9b3a17cbfdf14a8cd4dbd0b
jearbecerro <jearbecerro@gmail.com>
2025-07-20 04:19:20 +0800

    chore: refactor exports

---
+++ src/context/index.ts
@@ -0,0 +1,2 @@
+export * from "./FormConfigContext";
+export * from "./FormInputRegistryContext";
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 export * from "./Form";
 export * from "./useForm";
 export * from "./types";
-
-// NEW: export providers/contexts for easy access
-export * from "./context/FormConfigContext";
-export * from "./context/FormInputRegistryContext";
+export * from "./context"; 
