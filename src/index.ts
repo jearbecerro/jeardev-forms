d25c4b7799d5f1e2590dad4ae6d168526f78b20a
jearbecerro <jearbecerro@gmail.com>
2025-07-20 04:06:58 +0800

    fix: index and version

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-export * from './Form';
-export * from './useForm';
-export * from './types';
+export * from "./Form";
+export * from "./useForm";
+export * from "./types";
+
+// NEW: export providers/contexts for easy access
+export * from "./context/FormConfigContext";
+export * from "./context/FormInputRegistryContext";
